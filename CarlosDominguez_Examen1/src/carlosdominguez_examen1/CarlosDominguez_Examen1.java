b826cdd17d65fb40a33624ed250e6096bae6f1ff
cadodominguez <itzkarurosu@gmail.com>
2024-02-15 17:45:19 -0600

    Hecho el 4 y la estructura del programa

--- CarlosDominguez_Examen1/src/carlosdominguez_examen1/CarlosDominguez_Examen1.java
+++ CarlosDominguez_Examen1/src/carlosdominguez_examen1/CarlosDominguez_Examen1.java
@@ -1,20 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package carlosdominguez_examen1;
 
-/**
- *
- * @author USUARIO
- */
+package carlosdominguez_examen1;
+import java.util.Scanner;
 public class CarlosDominguez_Examen1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner brazil = new Scanner (System.in);
+        String Historialdebusqueda = "";
+        boolean out = true;
+        do {
+            System.out.println("********MENU**********");    
+            System.out.println("1. Ejercios de String ");
+            System.out.println("2. Respuestas a preguntas ");
+            System.out.println("3. Mostrar el historial del Menu ");
+            System.out.println("4. Salir");
+            int opcion = brazil.nextInt();
+            switch (opcion) {
+                case 1:
+                    Historialdebusqueda += "-Ejercicos de String";
+                    boolean out2 = true;
+                    while (out2 == true) {
+                        System.out.println("*******Submenu********");
+                        System.out.println("1. Ejercicio 1 String");
+                        System.out.println("2. Ejercicio 2 String ");
+                        System.out.println("3. Salir del Submenu");
+                        int opcion2 = brazil.nextInt();
+                        switch (opcion2) {
+                            case 1:
+                                Historialdebusqueda += "-Ejercicio 1 String";
+                                
+                                break;
+                            case 2:
+                                Historialdebusqueda += "-Ejercicio 2 String";
+                                
+                                break;
+                            case 3:
+                                out2=false;
+                                System.out.println("Saliendo del submenu.....");
+                                break;    
+                            default:
+                                System.out.println("La opcion no es valida");
+                        }
+                    }
+                    
+                    break;
+                case 2:
+                    Historialdebusqueda += "-Respuestas de Preguntas";
+
+                    
+                    break;
+                case 3:
+                    Historialdebusqueda += "-Mostrar el historial del menu";
+                    System.out.println(Historialdebusqueda);
+                    break;
+                case 4:
+                    System.out.println("Saliendo.......");
+                    out = false;
+                    break;    
+                default:
+                    System.out.println("La opcion no es valida");
+            }
+        } while (out);
     }
     
 }
